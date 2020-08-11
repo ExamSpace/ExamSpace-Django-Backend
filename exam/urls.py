8b607f806bef6e4cc914ba3ccf43f80cc1056132
TariqueNasrullah <nasrullahtarique@gmail.com>
2020-08-12 02:04:01 +0600

    rohan's overview

--- exam/urls.py
+++ exam/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import ExamsListView
+from .views import ExamsListView, ExamDetailView
 
 urlpatterns = [
     path('list', ExamsListView.as_view()),
+    path('<int:id>', ExamDetailView.as_view()),
 ]
--- exam/views.py
+++ exam/views.py
@@ -3,11 +3,24 @@ from django.shortcuts import render
 from .models import Exam, Question
 from .serializers import ExamSerializer, QuestionSerializer
 
-from rest_framework.generics import GenericAPIView, ListCreateAPIView, RetrieveUpdateDestroyAPIView, ListAPIView
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework import permissions
+
+
+class IsAdminOrReadOnly(permissions.BasePermission):
+    def has_permission(self, request, view):
+        is_admin = request.user and request.user.is_superuser
+        return request.method in permissions.SAFE_METHODS or is_admin
 
 
 class ExamsListView(ListCreateAPIView):
     serializer_class = ExamSerializer
     queryset = Exam.objects.all()
     permission_classes = (permissions.IsAuthenticatedOrReadOnly, )
+
+
+class ExamDetailView(RetrieveUpdateDestroyAPIView):
+    serializer_class = ExamSerializer
+    queryset = Exam.objects.all()
+    lookup_field = "id"
+    permission_classes = (IsAdminOrReadOnly, )
