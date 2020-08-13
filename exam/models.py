38931e437318be98e9ae3d78ad9877e9aed9fe0d
ashiqurrah7 <ashiqur.rahm@hotmail.com@hotmail.com>
2020-08-14 04:33:41 +0600

    Fixed Models

--- exam/models.py
+++ exam/models.py
@@ -1,15 +1,55 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+from datetime import date
 
 class Exam(models.Model):
-    title = models.CharField(max_length=250)
-    duration = models.FloatField(default=0.0)
+    name = models.CharField(max_length=255)
+    instruction = models.TextField()
+    duration = models.IntegerField()
+    start_date = models.DateField(default=date.today())
+    end_date = models.DateField(default=date.today())
+    passing_percent = models.IntegerField()
+    negative_marking = models.CharField(max_length=3)
+    negative_marks = models.FloatField()
+    marks = models.FloatField()
+    attempt_count = models.IntegerField()
+    declare_result = models.CharField(max_length=3)
+    finish_result = models.CharField(max_length=1)
+    ques_random = models.CharField(max_length=1)
+    paid_exam = models.CharField(max_length=1)
+    amount = models.DecimalField(max_digits=10, decimal_places=0, )
+    status = models.CharField(max_length=10)
+    user_id = models.IntegerField()
+    finalized_time = models.DateField(default=date.today())
+    created = models.DateField(default=date.today())
+    modified = models.DateField(default=date.today())
+    def __str__(self):
+        return self.name
 
 
 class Question(models.Model):
-    question = models.CharField(max_length=500)
+    qtype_id = models.IntegerField()
+    img = models.CharField(max_length=200)
+    qtag_id = models.IntegerField()
+    question = models.TextField()
+    option1 = models.TextField()
+    option2 = models.TextField()
+    option3 = models.TextField()
+    option4 = models.TextField()
+    option5 = models.TextField()
+    option6 = models.TextField()
+    marks = models.FloatField()
+    negative_marks = models.FloatField()
+    hint = models.TextField()
+    explanation = models.TextField()
+    explort = models.TextField()
+    answer = models.CharField(max_length=15)
+    true_false = models.CharField(max_length=5)
+    fill_blank = models.CharField(max_length=100)
+    status = models.CharField(max_length=3)
     exam = models.ForeignKey(to=Exam, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.question
 
 
 class Enrollment(models.Model):
