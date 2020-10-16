104c11a63af45a82039c270585cb0c9089c8f871
Zunaid Amin Enan <zunaid321@gmail.com>
2020-10-17 03:35:40 +0600

    Import/Export Working

--- exam/admin.py
+++ exam/admin.py
@@ -1,11 +1,23 @@
 from django.contrib import admin
+from import_export.admin import ImportExportModelAdmin
+from import_export import resources
 from .models import *
 
 admin.site.register(Enrollment)
 admin.site.register(Started)
 admin.site.register(Exam)
-admin.site.register(Question)
 admin.site.register(Answered)
 admin.site.register(Subject)
 admin.site.register(Mark)
 
+from import_export import resources
+
+class QuestionResource(resources.ModelResource):
+
+    class Meta:
+        model = Question
+
+class QuestionAdmin(ImportExportModelAdmin):
+    resource_class = QuestionResource
+
+admin.site.register(Question, QuestionAdmin)
